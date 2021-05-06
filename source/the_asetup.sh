a657faa3276e50f575ed05e2b38161972122dc4f
Kurt Brendlinger <kurt.brendlinger@cern.ch>
2021-05-06 17:03:30 +0200

    Update the asetup

--- source/the_asetup.sh
+++ source/the_asetup.sh
@@ -2,15 +2,16 @@
 # usage: source the_asetup.sh (from the source directory)
 #
 
-asetup AnalysisBase,21.2.56,here
+#asetup AnalysisBase,21.2.56,here
+asetup Athena,master,2021-04-26T2101,here
 
 doExtras () {
     export MYCMAKE_TMP=(${CMAKE_PREFIX_PATH//:/ })
     export CMAKE_ATLAS=$MYCMAKE_TMP # not sure why this is necessary.
-    if [ -a $TestArea/../build/$AnalysisBase_PLATFORM/setup.sh ]
+    if [ -a $TestArea/../build/$Athena_PLATFORM/setup.sh ]
     then
-        echo source $TestArea/../build/$AnalysisBase_PLATFORM/setup.sh
-        source $TestArea/../build/$AnalysisBase_PLATFORM/setup.sh
+        echo source $TestArea/../build/$Athena_PLATFORM/setup.sh
+        source $TestArea/../build/$Athena_PLATFORM/setup.sh
     fi
     export me=`pwd`
 }
@@ -19,7 +20,7 @@ mymake () {
     echo doing Athena-aware make
     sleep 1
     cd $TestArea/../build
-    cmake -DATLAS_PACKAGE_FILTER_FILE=../source/package_filters.txt ../source/
+    cmake -DATLAS_PACKAGE_FILTER_FILE=../source/package_filters.txt cmake ../source/athena/Projects/WorkDir/
 }
 
 mybuild () {
@@ -28,8 +29,8 @@ mybuild () {
     cd $TestArea/../build
     cmake --build .
     if [[ -z "$WorkDir_SET_UP" ]] ; then 
-        echo source $TestArea/../build/$AnalysisBase_PLATFORM/setup.sh
-        source $TestArea/../build/$AnalysisBase_PLATFORM/setup.sh
+        echo source $TestArea/../build/$Athena_PLATFORM/setup.sh
+        source $TestArea/../build/$Athena_PLATFORM/setup.sh
     fi
 }
 
