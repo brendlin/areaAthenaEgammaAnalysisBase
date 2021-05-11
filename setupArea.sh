d6934b31e61e36998dfc2ddc13581c70d728ca88
Kurt Brendlinger <kurt.brendlinger@cern.ch>
2021-05-11 11:23:07 +0200

    New command for checking out athena

--- setupArea.sh
+++ setupArea.sh
@@ -16,10 +16,10 @@ if [[ ! -d source/athena ]]; then
     echo checking out athena
     mkdir -p source/athena
     cd source/athena
-    git atlas init-workdir -b 21.2 ssh://git@gitlab.cern.ch:7999/${GIT_USER}/athena.git . \
-        -p ElectronPhotonShowerShapeFudgeTool ElectronPhotonSelectorTools
+    git atlas init-workdir ssh://git@gitlab.cern.ch:7999/${GIT_USER}/athena.git . \
+        -p egammaValidation egammaAlgs InDetConversionFinderTools InDetRecExample InDetTrackSelectorTool
     # git atlas addpkg PhotonEfficiencyCorrection
-    git remote add egammaOrigin ssh://git@gitlab.cern.ch:7999/ATLAS-EGamma/athena.git
+    # git remote add egammaOrigin ssh://git@gitlab.cern.ch:7999/ATLAS-EGamma/athena.git
     git fetch upstream
     cd -
 fi
