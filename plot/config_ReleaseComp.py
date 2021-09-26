be56540ee03224f3166f51af6940147fd2b99b08
brendlin <kurt.brendlinger@cern.ch>
2021-09-26 22:54:44 +0200

    Add a release comparison script and associated config file

---
+++ plot/config_ReleaseComp.py
@@ -0,0 +1,176 @@
+
+# for use with cutcomparisons:
+doCompareAllConversions = True
+
+treename = 'CollectionTree'
+
+doReleaseComparison = True
+
+import re
+import ROOT
+ROOT.gStyle.SetNumberContours(20)
+
+test_label = 'test'
+ref_label = 'ref'
+
+variables = [
+    # 'mu',
+    # 'trueR',
+    'abs(truthEta)',
+    # 'truthPt',
+    # 'recoPt',
+    # 'eProbabilityHT_trans_0',
+    # 'eProbabilityHT_trans_0*(trueR > 0)',
+    # 'mu*(trueR > 0)',
+    # 'nTRT_0*(trueR > 0)',
+    # 'eProbabilityHT_trans_0*(trueR <= 0)',
+    # 'mu*(trueR <= 0)',
+    # 'nTRT_0*(trueR <= 0)',
+    # 'abs(recoEta)',
+    # 'abs(trkEta_0)',
+    # 'nSi_0',
+    # 'nSi_1',
+    # 'eProbabilityHT_0',
+    # 'eProbabilityHT_1',
+    # 'max(eProbabilityHT_0,0)',
+    # 'max(eProbabilityHT_1,0)',
+    # 'nTRT_0',
+    # 'nTRT_1',
+    # 'precHitFrac_0',
+    # 'precHitFrac_1',
+    # 'nTRT_0 - 0.2*mu',
+    # 'max(eProbabilityNN_0,0)',
+    # 'max(eProbabilityNN_1,0)',
+    # 'eProbabilityHT_0*(nSi_0 > 0) + eProbabilityHT_1*(nSi_1 > 0)', # Si track of Si-TRT
+    # 'eProbabilityHT_0*(nSi_0 == 0) + eProbabilityHT_1*(nSi_1 == 0)', # TRT track of Si-TRT
+    # 'trkPt_0/1000.',
+]
+
+# On top of what is specified in cutcomparisons, these cuts are also applied to all entries in
+# the cutcomparisons dict
+cuts = [
+    'trueR > 0',    # all truth converted photons
+    #'trueR <= 0',    # all truth unconverted photons
+    'truthPt > 15.', # in GeV
+    # 'recoPt > 0',                 # all reco photons
+    # 'convType > 0',               # reco converted
+    # 'convType == 2',              # Single TRT
+    # '(nSi_0 <= 0 && nSi_1 <= 0)', # TRT-only
+    # '((nSi_0 > 0 || nSi_1 > 0) && (!(nSi_0 > 0 && nSi_1 > 0)))', # Si-TRT
+    # 'abs(recoEta)*1000 < 1.0',
+    # 'convType == 3', # 2Si
+    # 'convType == 4', # 2TRT
+    # 'convType == 5', # SiTRT
+    # 'abs(recoEta) < 1.2',
+    # 'abs(recoEta) > 0.7',
+    # 'precHitFrac_0 > 0.3',
+    # '(eProbabilityHT_0 > 0.75 || nTRT_0 < 0)',
+]
+
+cuts_all_truth_conv_photons = []
+cuts_reco_conv_photon = ['recoPt > 0','convType > 0']
+cuts_single_si        = ['recoPt > 0','convType > 0','convType == 1']
+cuts_single_trt       = ['recoPt > 0','convType > 0','convType == 2']
+cuts_2si              = ['recoPt > 0','convType > 0','convType == 3']
+cuts_2trt             = ['recoPt > 0','convType > 0','convType == 4']
+cuts_1si_and_sitrt    = ['recoPt > 0','convType > 0','(convType == 1 || convType == 5)']
+cuts_1trt_and_2trt    = ['recoPt > 0','convType > 0','(convType == 2 || convType == 4)']
+cuts_sitrt            = ['recoPt > 0','convType > 0','convType == 5']
+
+from collections import OrderedDict
+cutcomparisons = OrderedDict()
+cutcomparisons['all truth conv Photons' ] = []
+cutcomparisons['Reco Conv Photon'] = cuts_reco_conv_photon
+# cutcomparisons['Single Si'       ] = cuts_single_si
+#cutcomparisons['Single TRT'      ] = cuts_single_trt
+# cutcomparisons['2Si'             ] = cuts_2si
+#cutcomparisons['2TRT'            ] = cuts_2trt
+# cutcomparisons['1Si+SiTRT'       ] = cuts_1si_and_sitrt
+cutcomparisons['1TRT+2TRT'       ] = cuts_1trt_and_2trt
+# cutcomparisons['SiTRT'           ] = cuts_sitrt
+
+# To get consistent plot colors (% is wildcard)
+from ROOT import kGray,kBlack,kBlue,kRed,kGreen,kOrange,kCyan
+colors = { 
+    '%all truth conv Photons%':kGray,
+    '%all truth Photons%':kGray,
+    '%Reco Conv Photon%' :kBlack,
+    '%Single Si%'        :kBlue+2,
+    '%Single TRT%'       :kRed+2,
+    '%2Si%'              :kGreen+2,
+    '%2TRT%'             :kOrange+2,
+    '%SiTRT%'            :kCyan+2,
+    '%Unconv%'           :kOrange+3,
+}
+
+# Formatting the histogram ranges / x-axis labels
+histformat = {
+    'trueR':[1085,0,1085,'Truth Radius [mm]'],
+    'mu':[45,0,90,'#mu'],
+    'mu*(trueR > 0)':[17,5,90,'#mu'],
+    'mu*(trueR <= 0)':[17,5,90,'#mu'],
+    'eProbabilityHT_0':[20,0,1,'eProbabilityHT_0'],
+    'eProbabilityHT_trans_0':[40,-0.1,1.5,'eProbabilityHT_trans_0'],
+    'eProbabilityHT_trans_0*(trueR > 0)':[50,-0.1,1,'eProbabilityHT_trans_0'],
+    'eProbabilityHT_trans_0*(trueR <= 0)':[50,-0.1,1,'eProbabilityHT_trans_0'],
+    'eProbabilityHT_1':[20,0,1,'eProbabilityHT_1'],
+    'max(eProbabilityHT_0,0)':[20,0,1,'eProbabilityHT_0'],
+    'max(eProbabilityHT_1,0)':[20,0,1,'eProbabilityHT_1'],
+    'max(eProbabilityNN_0,0)':[20,0,1,'eProbabilityNN_0'],
+    'max(eProbabilityNN_1,0)':[20,0,1,'eProbabilityNN_1'],
+    'nSi_0':[20,0,20,'nSi_0'],
+    'nSi_1':[20,0,20,'nSi_1'],
+    'nTRT_0':[50,-0.5,49.5,'nTRT_0'],
+    'nTRT_0*(trueR > 0)':[25,0,50,'nTRT_0'],
+    'nTRT_0*(trueR <= 0)':[25,0,50,'nTRT_0'],
+    'nTRT_1':[50,0,50,'nTRT_1'],
+    'truthPt':[2985,15,3000,'truth p_{T} [GeV]'],
+    'abs(recoEta)':[100,0,2.5,'reco |#eta|'],
+    'abs(truthEta)':[25,0,2.5,'truth |#eta|'],
+    'abs(trkEta_0)':[50,0,2.5,'track 0 |#eta|'],
+    'abs(trkEta_1)':[50,0,2.5,'track 1 |#eta|'],
+    'eProbabilityHT_0*(nSi_0 > 0) + eProbabilityHT_1*(nSi_1 > 0)':[20,0,1,'eProbabilityHT Si'],
+    'eProbabilityHT_0*(nSi_0 == 0) + eProbabilityHT_1*(nSi_1 == 0)':[20,0,1,'eProbabilityHT TRT'],
+    'nTRT_0 - 0.2*mu':[25,0,45,'mu-corrected nTRT'],
+    'precHitFrac_0':[20,0,1,'precHitFrac'],
+    'precHitFrac_1':[20,0,1,'precHitFrac (trk1)'],
+    'trkPt_0/1000.':[30,0,100,'track p_{T} [GeV]'],
+}
+
+# This dict gives you the opportunity to make variable bin widths, as long as they are consistent
+# with the bin edges specified in "histformat"
+rebin = {
+    'trueR':[0, 50, 89, 123, 170, 210, 250, 299, 335, 371, 443, 514, 554, 800, 1085],
+    'truthPt':[15,20,25,30,40,50,60,70,80,90,100,150,200,
+               250,300,400,500,600,700,800,900,1000,2000,3000],
+    #'abs(truthEta)':[0.0, 0.75, 1.2, 2.5],
+    #'abs(truthEta)':[0.0, 2.5],
+}
+
+# Here you can do any last-minute manipulations of the plot
+def afterburner(can) :
+    import PlotFunctions as plotfunc
+
+    # Make the "reference, test" legend
+    file_leg = ROOT.TLegend(0.19,0.75,0.49,0.87)
+    file_leg.SetName('file_titles_%s'%(can.GetName()))
+    file_leg.SetFillStyle(0)
+    leghist_ref = ROOT.TH1F()
+    leghist_ref.SetMarkerColor(ROOT.kGray)
+    leghist_ref.SetLineColor(ROOT.kGray)
+    leghist_ref.SetMarkerStyle(24)
+    file_leg.AddEntry(leghist_ref,ref_label,'pe')
+    leghist_test = ROOT.TH1F()
+    leghist_test.SetMarkerColor(ROOT.kGray)
+    leghist_test.SetLineColor(ROOT.kGray)
+    leghist_test.SetMarkerStyle(20)
+    file_leg.AddEntry(leghist_test,test_label,'pe')
+    can.cd()
+    if plotfunc.GetTopPad(can) :
+        plotfunc.GetTopPad(can).cd()
+    file_leg.Draw()
+    plotfunc.tobject_collector.append(leghist_ref)
+    plotfunc.tobject_collector.append(leghist_test)
+    plotfunc.tobject_collector.append(file_leg)
+
+    return
---
+++ plot/releasecomparisons.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+
+##
+## This macro offers a way to compare different releases.
+## To use it, make a config file and define inside a list called "cutcomparisons", e.g. to compare
+## "CutSet1" (comprised of cut1a, cut1b, cut1c) and "CutSet2" (comprised of cut2a, cut2b, cut2c) do:
+## cutcomparisons = [ ['CutSet1',['cut1a','cut1b','cut1c']], ['CutSet2',['cut2a','cut2b','cut2c']] ]
+##
+
+import ROOT,sys,os
+
+# This only works with python2
+sys.path.insert(0, "%s/../genericUtils/python"%(os.path.dirname(os.path.abspath(__file__))))
+import TAxisFunctions as taxisfunc
+import PyAnalysisPlotting as anaplot
+import PlotFunctions as plotfunc
+
+# import TAxisFunctions as taxisfunc
+# import PyAnalysisPlotting as anaplot
+# import PlotFunctions as plotfunc
+import copy
+
+def DrawHistosCutComparison(variable,options,ref_hists=[],test_hists=[],name='') :
+    #
+    # Clean up name
+    #
+    canname = anaplot.CleanUpName(variable)
+
+    #
+    # stack, before adding SUSY histograms
+    #
+    if not options.ratio :
+        can = ROOT.TCanvas(canname,canname,800,600)
+    else :
+        can = plotfunc.RatioCanvas(canname,canname,500,500)
+
+    if ref_hists :
+        for i,h in enumerate(ref_hists) :
+
+            if options.efficiency and i == 0 :
+                continue
+
+            if options.efficiency :
+                h.Divide(ref_hists[0])
+
+            plotfunc.AddHistogram(can,h,'pE1') # pE1
+
+    if test_hists :
+        for i,h in enumerate(test_hists) :
+
+            if options.efficiency and i == 0 :
+                continue
+
+            if options.efficiency :
+                h.Divide(test_hists[0])
+
+            if options.ratio :
+                plotfunc.AddRatio(can,h,ref_hists[i])
+            else :
+                plotfunc.AddHistogram(can,h)
+
+    if not options.ratio :
+        plotfunc.FormatCanvasAxes800600(can)
+    else :
+        plotfunc.FormatCanvasAxes(can)
+
+    text_lines = [plotfunc.GetSqrtsText(13)]
+    if options.fb > 0 :
+        text_lines += [plotfunc.GetLuminosityText(options.fb)]
+    text_lines += [plotfunc.GetAtlasInternalText()]
+    if hasattr(options,'plottext') and options.plottext :
+        text_lines += options.plottext
+
+    if options.log :
+        if options.ratio :
+            if taxisfunc.MinimumForLog(can.GetPrimitive('pad_top')) > 0 :
+                can.GetPrimitive('pad_top').SetLogy()
+        else :
+            if taxisfunc.MinimumForLog(can) > 0 :
+                can.SetLogy()
+
+    if options.ratio :
+        #plotfunc.DrawText(can,text_lines,0.2,0.65,0.5,0.90,totalentries=4)
+        plotfunc.MakeLegend(can,0.53,0.65,0.92,0.90,totalentries=5,ncolumns=1,skip=['remove me'])
+    else :
+        #plotfunc.DrawText(can,text_lines,0.2,0.75,0.5,0.94,totalentries=4,textsize=27)
+        plotfunc.MakeLegend(can,0.53,0.75,0.94,0.92,totalentries=4,ncolumns=1,skip=['remove me'],
+                            textsize=27)
+
+    ylabel = 'entries (normalized)' if options.normalize else 'entries'
+    if options.efficiency :
+        ylabel = 'efficiency'
+    plotfunc.SetAxisLabels(can,options.xlabel.get(variable),ylabel)
+    plotfunc.AutoFixAxes(can)
+
+    if not options.log :
+        if can.GetPrimitive('pad_top') :
+            plotfunc.AutoFixYaxis(can.GetPrimitive('pad_top'),minzero=True)
+        else :
+            plotfunc.AutoFixYaxis(can,minzero=True)
+
+    return can
+
+#-------------------------------------------------------------------------
+def main(options,args) :
+
+    mystyle = plotfunc.SetupStyle()
+
+    files_test,trees_test,keys_test = anaplot.GetTreesFromFiles(options.test     ,treename=options.treename,xAODInit=options.xAODInit)
+    files_ref ,trees_ref ,keys_ref  = anaplot.GetTreesFromFiles(options.reference,treename=options.treename,xAODInit=options.xAODInit)
+
+    # A bit of a hack to get the test and ref labels properly in the code:
+    options.labels = dict()
+    for k in keys_test :
+        options.labels[k] = options.test_label
+    for k in keys_ref :
+        options.labels[k] = options.ref_label
+
+    scales_ref = anaplot.GetScales(files_ref,trees_ref,keys_ref,options)
+
+    cans = []
+
+    # get the histograms from the files
+    for v in options.variables :
+        ref_hists = []
+        test_hists = []
+
+        for cname in options.cutcomparisons.keys() :
+            cutcomp = options.cutcomparisons[cname]
+            inputname = '%s_%s'%(v,cname)
+
+            weight = options.weight
+
+            if type(weight) == type(dict()) :
+                weight = weight[cname]
+
+            if ''.join(options.cuts+cutcomp+options.truthcuts) :
+                weight = (weight+'*(%s)'%(' && '.join(options.cuts+cutcomp+options.truthcuts).lstrip('& ').rstrip('& '))).lstrip('*')
+
+            dweight = '' # weight value (and cuts) applied to data
+            if ''.join(options.cuts+cutcomp[1:]+options.blindcut) :
+                dweight = '('+' && '.join(options.cuts+cutcomp+options.blindcut).lstrip('& ').rstrip('& ')+')'
+
+            if options.test :
+                test_hists_tmp = anaplot.GetVariableHistsFromTrees(trees_test,keys_test,v,dweight,options,files=files_test,inputname=inputname)
+                test_hists_tmp = anaplot.MergeSamples(test_hists_tmp,options,requireFullyMerged=True)
+
+                for d in test_hists_tmp :
+                    d.SetTitle(cname)
+                    test_hists.append(d)
+
+            if options.reference :
+                ref_hists_tmp = anaplot.GetVariableHistsFromTrees(trees_ref,keys_ref,v,weight,options,scales=scales_ref,files=files_ref,inputname=inputname)
+                ref_hists_tmp = anaplot.MergeSamples(ref_hists_tmp,options)
+                anaplot.SetLegendLabels(ref_hists_tmp,options)
+
+                for s in ref_hists_tmp :
+                    s.SetTitle(('%s, %s'%(s.GetTitle(),cname)).lstrip(' ,'))
+                    ref_hists.append(s)
+
+        if options.normalize :
+            for hist in test_hists + ref_hists :
+                if not hist : continue
+                hist.Scale(1/float(hist.Integral()))
+
+        if options.customnormalize :
+            options.customnormalize(v,ref_hists=ref_hists,data_hist=data_hist)
+
+        # Hack to turn off labels (since they are already set) before another round of
+        # PrepareSignalHistos.
+        tmp_options = copy.copy(options)
+        tmp_options.labels = {}
+        anaplot.PrepareSignalHistos(ref_hists+test_hists,tmp_options)
+        for h in ref_hists :
+            h.SetMarkerStyle(24)
+            h.SetTitle('remove')
+
+        ## Special canvas:
+        cans.append(DrawHistosCutComparison(v,options,ref_hists=ref_hists,test_hists=test_hists))
+
+    if options.afterburner :
+        for can in cans :
+            options.afterburner(can)
+
+    anaplot.UpdateCanvases(cans,options)
+
+    if options.xAODInit :
+        ROOT.xAOD.ClearTransientTrees()
+
+    if not options.batch :
+        import code
+        code.interact(banner='Pausing... Press Contol-D to exit.',local=locals())
+
+    anaplot.doSaving(options,cans)
+
+    print('done.')
+    return
+
+if __name__ == '__main__':
+
+    p = anaplot.TreePlottingOptParser()
+
+    p.p.remove_option('--bkgs')
+    p.p.add_option('--reference',type='string',default='',dest='reference',help='reference release')
+    p.p.add_option('--test'     ,type='string',default='',dest='test'     ,help='test release')
+    p.p.add_option('--efficiency',action='store_true',default=False,dest='efficiency',help='Plot efficiencies')
+
+    options,args = p.parse_args()
+
+    if options.ratio :
+        print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+        print('%%','Taking the ratio of TEST divided by REF')
+        print('%%',' and plotting in the bottom pad.')
+        print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+
+    if options.efficiency :
+        print ('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+        print ('%%','Important! Plotting the efficiency with the FIRST entry in cutcomparisons')
+        print ('%%',' as the denominator!')
+        print ('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+
+    if hasattr(options.usermodule,'cutcomparisons') :
+        options.cutcomparisons = options.usermodule.cutcomparisons
+
+    if hasattr(options.usermodule,'test_label') :
+        options.test_label = options.usermodule.test_label
+
+    if hasattr(options.usermodule,'ref_label') :
+        options.ref_label = options.usermodule.ref_label
+
+    if not options.variables :
+        print ('Error! Please specify a variable!')
+        sys.exit()
+
+    main(options,args)
+
